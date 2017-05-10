b322ff5bea230168faccf8216aa40a7853dd6855
Ben Fairless <ben.fairless@landregistry.gov.uk>
2017-05-10 10:43:14 +0100

    Update eap_slave.pp

--- site/profiles/manifests/eap_slave.pp
+++ site/profiles/manifests/eap_slave.pp
@@ -2,6 +2,15 @@
 
 class profiles::eap_slave(){
 
+  # Assume that if we have multiple interfaces that eth1 is the data
+  if ($::ipaddress_eth1) {
+    $mgmt_addr = $::ipaddress_eth0
+    $data_addr = $::ipaddress_eth1
+  } else {
+    $mgmt_addr = $::ipaddress_eth0
+    $data_addr = $::ipaddress_eth0
+  }
+
   class { '::wildfly':
     distribution => 'jboss-eap',
     user         => 'jboss-eap',
@@ -11,5 +20,9 @@ class profiles::eap_slave(){
     java_home    => '/usr',
     mode         => 'domain',
     host_config  => 'host-slave.xml',
+    properties   => {
+      'jboss.bind.address'            => $data_addr,
+      'jboss.bind.address.management' => $mgmt_addr,
+    }
   }
 }
