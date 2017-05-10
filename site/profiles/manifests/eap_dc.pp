25f82ebbf3b9fa56efa018fe3afe3c34356da1c6
Ben Fairless <ben.fairless@landregistry.gov.uk>
2017-05-10 10:45:46 +0100

    Update eap_dc.pp

--- site/profiles/manifests/eap_dc.pp
+++ site/profiles/manifests/eap_dc.pp
@@ -2,6 +2,15 @@
 
 class profiles::eap_dc(){
 
+  # Assume that if we have multiple interfaces that eth1 is the data
+  if ($::ipaddress_eth1) {
+    $mgmt_addr = $::ipaddress_eth0
+    $data_addr = $::ipaddress_eth1
+  } else {
+    $mgmt_addr = $::ipaddress_eth0
+    $data_addr = $::ipaddress_eth0
+  }
+
   class { '::wildfly':
     distribution => 'jboss-eap',
     user         => 'jboss-eap',
@@ -11,7 +20,13 @@ class profiles::eap_dc(){
     java_home    => '/usr',
     mode         => 'domain',
     host_config  => 'host-master.xml',
+    properties   => {
+      'jboss.bind.address'            => $data_addr,
+      'jboss.bind.address.management' => $mgmt_addr,
+    }
   }
+  
   $dc_server_groups = hiera_hash('wildfly::dc_server_groups',false)
   create_resources('wildfly::domain::server_group', $dc_server_groups)
+
 }
