3de5352b1e267613d404d92a683c083f08fd5dec
Venu Nandipati <nvmadhav@hotmail.com>
2017-06-15 10:36:16 +0100

    domain control configuration to include the server creation undertow config datasource creation naming and binding creation via hiera

--- site/profiles/manifests/eap_dc.pp
+++ site/profiles/manifests/eap_dc.pp
@@ -4,19 +4,62 @@ class profiles::eap_dc(){
 
   class { '::wildfly':
     distribution => 'jboss-eap',
-    user         => 'jboss-eap',
-    group        => 'jboss-eap',
-    dirname      => '/opt/jboss-eap',
     console_log  => '/var/log/jboss-eap/console.log',
     java_home    => '/usr',
     mode         => 'domain',
     host_config  => 'host-master.xml',
   }
+  $modules = [ "${wildfly::dirname}/modules/com", "${wildfly::dirname}/modules/com/ibm","${wildfly::dirname}/modules/com/ibm/main",
+              "${wildfly::dirname}/modules/com/oracle","${wildfly::dirname}/modules/com/oracle/main"]
+  file { $modules:
+    ensure  => 'directory',
+    owner   => $wildfly::user,
+    group   => $wildfly::group,
+    require => Class['::wildfly']
+  }
+  file { "${wildfly::dirname}/modules/com/ibm/main/module.xml":
+    ensure  => present,
+    owner   => $wildfly::user,
+    group   => $wildfly::group,
+    source  => 'puppet:///modules/profiles/jboss-eap/ibm/module.xml',
+    recurse => true,
+    require => File[$modules]
+  }
+  file { "${wildfly::dirname}/modules/com/ibm/main/db2jcc_license_cisuz.jar":
+    ensure  => present,
+    owner   => $wildfly::user,
+    group   => $wildfly::group,
+    source  => 'puppet:///modules/profiles/jboss-eap/ibm/db2jcc_license_cisuz.jar',
+    recurse => true,
+    require => File[$modules],
+  }
+  file { "${wildfly::dirname}/modules/com/ibm/main/db2jcc4.jar":
+    ensure  => present,
+    owner   => $wildfly::user,
+    group   => $wildfly::group,
+    source  => 'puppet:///modules/profiles/jboss-eap/ibm/module.xml',
+    recurse => true,
+    require => File[$modules],
+  }
+  file { "${wildfly::dirname}/modules/com/oracle/main/module.xml":
+    ensure  => present,
+    owner   => $wildfly::user,
+    group   => $wildfly::group,
+    source  => 'puppet:///modules/profiles/jboss-eap/oracle/module.xml',
+    recurse => true,
+    require => File[$modules]
+  }
+  file { "${wildfly::dirname}/modules/com/oracle/main/ojdbc7.jar":
+    ensure  => present,
+    owner   => $wildfly::user,
+    group   => $wildfly::group,
+    source  => 'puppet:///modules/profiles/jboss-eap/oracle/module.xml',
+    recurse => true,
+    require => File[$modules],
+  }
+
   $dc_server_groups = hiera_hash('wildfly::dc_server_groups',false)
   create_resources('wildfly::domain::server_group', $dc_server_groups)
-
-  $servers = hiera_hash('wildfly::servers',false)
-  create_resources('wildfly::resource', $servers)
 
   $mod_cluster_config=hiera_hash('wildfly::mod_cluster_config',false)
   create_resources('wildfly::resource',$mod_cluster_config)
@@ -26,6 +69,18 @@ class profiles::eap_dc(){
 
   $default_undertow= hiera_hash('wildfly::default_undertow',false)
   create_resources('wildfly::resource',$default_undertow)
+
+  $driver_reg=hiera_hash('wildfly::driver_reg',false)
+  create_resources('wildfly::datasources::driver', $driver_reg)
+
+  $datasources = hiera_hash('wildfly::datasources',false)
+  create_resources('wildfly::datasources::xa_datasource',$datasources)
+
+  $naming = hiera_hash('wildfly::naming',false)
+  create_resources('wildfly::resource',$naming)
+
+  $servers = hiera_hash('wildfly::servers',false)
+  create_resources('wildfly::resource', $servers)
 
   $deployment = hiera_hash('wildfly::deployment',false)
   create_resources('wildfly::deployment', $deployment)
