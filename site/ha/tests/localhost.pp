7b469e24a9e10434a0aa0733639b004c6296c091
Ben Fairless <ben.fairless@landregistry.gov.uk>
2017-07-10 15:11:45 +0100

    Update localhost.pp

--- site/ha/tests/localhost.pp
+++ site/ha/tests/localhost.pp
@@ -7,26 +7,26 @@ class { 'ha':
       'healthcheck' => '/',
       'backends'    => [ 'localhost:8080', 'localhost:8081']
     },
-    'sticky-test'    => {
-      'port' => 5001,
-      'sticky' => true,
+    'sticky-test' => {
+      'port'        => 5001,
+      'sticky'      => true,
       'healthcheck' => '/health',
       'backends'    => [ 'localhost:8080', 'localhost:8081']
     },
     'tcp-test' => {
-      'port' => 5002,
-      'mode' => 'tcp',
-      'backends'    => [ 'localhost:8080', 'localhost:8081']
+      'port'     => 5002,
+      'mode'     => 'tcp',
+      'backends' => [ 'localhost:8080', 'localhost:8081']
     },
     'auth-test' => {
-      'port'        => 5003,
-      'auth_group'  => 'admin',
-      'backends'    => [ 'localhost:8080', 'localhost:8081']
+      'port'       => 5003,
+      'auth_group' => 'admin',
+      'backends'   => [ 'localhost:8080', 'localhost:8081']
     },
     'failover-test' => {
-      'port' => 5004,
-      'backends'    => [ 'localhost:8080', 'localhost:8081'],
-      'failover'    => [ 'localhost:8082', 'localhost:8083'],
+      'port'     => 5004,
+      'backends' => [ 'localhost:8080', 'localhost:8081'],
+      'failover' => [ 'localhost:8082', 'localhost:8083'],
     }
   },
   auth_users => {
