14371786f54de46058f3ed8b28517346c4952cf0
Sebastian Weavers <sebastian.weavers@landregistry.gov.uk>
2017-07-31 16:41:44 +0100

    Revert "removing unmanaged repos from yum"

--- site/profiles/manifests/pulp_repos.pp
+++ site/profiles/manifests/pulp_repos.pp
@@ -10,14 +10,4 @@ class profiles::pulp_repos(
     group   => 'root',
     mode    => '0644',
   }
-  ~>
-  file { '/etc/yum.repos.d/':
-    ensure  => 'directory',
-    recurse => true,
-    purge   => true,
-  }
-  ~>
-  exec { 'Clean Yum cache':
-    command => 'yum clean all'
-  }
 }
