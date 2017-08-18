4fe46ddfcb6da55b740554fdc6aeba4b8afcdd74
Sebastian Weavers <sebastian.weavers@landregistry.gov.uk>
2017-08-18 15:52:34 +0100

    new version of pulp and updated pulp profiles

--- site/profiles/manifests/pulp.pp
+++ site/profiles/manifests/pulp.pp
@@ -1,27 +1,39 @@
 class profiles::pulp(
   $rpmrepos = {},
-  $uname = undef,
-  $pword = undef,
+  $username = undef,
+  $password = undef,
   ){
 
   class { 'pulp::runstages':}
   class { '::pulp':
     broker_url          => $pulp::broker_url,
     messaging_url       => $pulp::messaging_url,
-    messaging_transport => $pulp::messaging_transport
+    messaging_transport => $pulp::messaging_transport,
     }
   class { '::pulp::consumer':
     host                => $pulp::consumer::host,
     messaging_host      => $pulp::consumer::messaging_host,
     messaging_transport => $pulp::consumer::messaging_transport,
-    messaging_port      => $pulp::consumer::messaging_port
+    messaging_port      => $pulp::consumer::messaging_port,
     }
-  class { '::pulp::admin':} ->
+  class { '::pulp::admin':
+    username => $pulp::admin::username,
+    password => $pulp::admin::password,
+  }
+  class { '::pulp::admin_conf':
+    username => $pulp::admin::username,
+    password => $pulp::admin::password,
+    stage    => 'pulp-admin-conf',
+  }
   class { '::pulp::login':
-    uname => $uname,
-    pword => $pword,
-    stage => 'justbefore',
-  } ->
+    username => $pulp::admin::username,
+    stage    => 'pulp-login',
+  }
+  class { '::pulp::admin_update':
+    username => $pulp::admin::username,
+    password => $pulp::admin::password,
+    stage    => 'pulp-user-update',
+  }
   class { '::pulp::reposetup':
     rpmrepos => $rpmrepos,
     stage    => 'last',
--- site/profiles/manifests/pulp_pusher.pp
+++ site/profiles/manifests/pulp_pusher.pp
@@ -1,17 +1,23 @@
-class profiles::pulp_pusher(
-  $uname = undef,
-  $pword = undef,
-  ){
+class profiles::pulp_pusher(){
 
+  class { 'pulp::runstages':}
   class { '::pulp::consumer':
     host                => $pulp::consumer::host,
     messaging_host      => $pulp::consumer::messaging_host,
     messaging_transport => $pulp::consumer::messaging_transport,
   }
 
-  class { '::pulp::admin':} ->
+  class { '::pulp::admin':
+  username => $pulp::admin::username,
+  password => $pulp::admin::password,
+  }
+  class { '::pulp::admin_conf':
+    username => $pulp::admin::username,
+    password => $pulp::admin::password,
+    stage    => 'pulp-admin-conf',
+  }
   class { '::pulp::login':
-    uname => $uname,
-    pword => $pword,
+    username => $pulp::admin::username,
+    stage    => 'pulp-login',
   }
 }
